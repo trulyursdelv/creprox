c635a2ab1513a0598b1b45f7bb74143025090efa
delv <151720755+trulyursdelv@users.noreply.github.com>
2025-05-23 15:38:03 +0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -16,21 +16,19 @@ module.exports = async (req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Origin", "*");
-  const url = `https:/${req.url}`;
   
-  const chunks = [];
-  req.on("data", chunk => chunks.push(chunk));
-  req.on("end", async () => {
-    const buffer = chunks.length > 0 ? Buffer.concat(chunks) : undefined;
-    
-    const data = await startTransaction(req.method, url, req.headers, buffer);
-    data.headers.forEach((v, k) => {
-      if(k == "content-type") return;
-      res.setHeader(k, v);
+  const url = `https:/${req.url}`;
+  return new Promise(resolve => {
+    const chunks = [];
+    req.on("data", chunk => chunks.push(chunk));
+    req.on("end", async () => {
+      const buffer = chunks.length > 0 ? Buffer.concat(chunks) : undefined;
+      
+      const data = await startTransaction(req.method, url, req.headers, buffer);
+      data.headers.forEach((v, k) => {
+        res.setHeader(k, v);
+      });
+      resolve(res.status(data.status).end(data.body));
     });
-    console.info("Status:", data.status, typeof data.status);
-    console.info("Body:", data.body, typeof data.body);
-    res.statusCode = data.status;
-    res.end(data.body);
   });
 }
