f3ef489a0e23d99cc55b4f39ba637b7e7d980e69
delv <151720755+trulyursdelv@users.noreply.github.com>
2025-05-23 15:56:46 +0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -12,10 +12,6 @@ async function startTransaction(method, url, headers, body) {
   });
 }
 
-/*async function startTransaction(method, url, headers, when) {
-  const body = !["GET", "OPTIONS"].includes(method) ? 
-}*/
-
 module.exports = async (req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Access-Control-Allow-Methods", "*");
@@ -39,18 +35,4 @@ module.exports = async (req, res) => {
     const data = await action.text();
     res.status(200).end(data.toString());
   });
-  req.on("error", () => {
-    res.status(200).end("oops!");
-  })
-  //const chunks = [];
-  /*req.on("data", chunk => chunks.push(chunk));
-  req.on("end", async () => {
-    const buffer = chunks.length > 0 ? Buffer.concat(chunks) : undefined;
-    
-    const data = await startTransaction(req.method, url, req.headers, buffer);
-    data.headers.forEach((v, k) => {
-      res.setHeader(k, v);
-    });
-    
-  });*/
 }
