163336074a4599f4a2824146bbadaebf5ada5135
delv <151720755+trulyursdelv@users.noreply.github.com>
2025-05-28 15:56:04 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-async function startTransaction(method, url, headers, body) {
-  return new Promise(async resolve => {
-    const req = await fetch(url, {
-      method, headers, body
-    });
-    const res = await req.text();
-    resolve({
-      status: req.status,
-      headers: req.headers,
-      body: res.toString()
-    });
-  });
-}
-
 module.exports = async (req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Access-Control-Allow-Methods", "*");
