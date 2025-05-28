a37cd05f97659986982a42e8248086e5cb2180e0
delv <151720755+trulyursdelv@users.noreply.github.com>
2025-05-28 16:51:32 +0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,26 +1,33 @@
 module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  
+  if(req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
   
   const url = `https:/${req.url}`;
   const chunks = [];
   req.on("data", chunk => chunks.push(chunk));
   req.on("end", async () => {
-    const body = Buffer.concat(chunks).toString();
+    const body = Buffer.concat(chunks);
     
+    const headers = { ...req.headers };
+    delete headers["host"];
+    delete headers["content-length"];
+
     const action = await fetch(url, {
       method: req.method,
-      headers: req.headers,
+      headers,
       body: chunks.length > 0 ? body : undefined
     });
     
-    action.headers.forEach((v, k) => {
-      res.setHeader(k, v);
+    action.headers.forEach((value, key) => {
+      res.setHeader(key, value);
     });
     
     const buffer = await action.arrayBuffer();
-    const data = Buffer.from(buffer);
-    res.status(action.status).end(data);
+    res.status(action.status).end(Buffer.from(buffer));
   });
-}
+};
